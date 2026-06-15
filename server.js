27a9c98080b4b066c17fb23ff0e52ef39394c501
Arshdadwal99 <112474379+Arshdadwal99@users.noreply.github.com>
2026-06-16 01:34:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 8000;
 
-const PORT = process.env.PORT || 8000;
-
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
