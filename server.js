471c77de2a3fc5966808353f8b7c5366e80081d2
Arshdadwal99 <112474379+Arshdadwal99@users.noreply.github.com>
2026-06-16 01:01:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hotel Booking Deployment Successful');
-}).listen(3000, () => {
+});
+
+server.listen(3000, () => {
     console.log('Server running on port 3000');
 });
